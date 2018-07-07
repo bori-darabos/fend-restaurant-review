2964b84da5145b4fec0360726ca62fd3a2c1b9e1
daborcsi <36795096+daborcsi@users.noreply.github.com>
2018-07-07 21:18:09 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,41 @@
+self.addEventListener('install', (event) => {
+	event.waitUntil(
+		caches.open('v1').then((cache) => {
+			return cache.addAll([
+				'/',	
+
+				'img/1.jpg',
+				'img/2.jpg',
+				'img/3.jpg',
+				'img/4.jpg',
+				'img/5.jpg',
+				'img/6.jpg',
+				'img/7.jpg',
+				'img/8.jpg',
+				'img/9.jpg',
+				'img/10.jpg',
+				'/data/restaurants.json'
+				'index.html',
+				'restaurant.html',
+				'css/styles.css',
+				'js/main.js',
+				'js/dbhelper.js',
+				'js/restaurant_info.js',
+
+			]);
+		})
+	);
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((respond) => {
+            return respond || fetch(event.request).then((response) => {
+                return caches.open('v1').then((cache) => {
+                    cache.put(event.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
